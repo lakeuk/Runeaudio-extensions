b9fe82046d61c0ebc68b7cf9ede3ce81829b8023
lakeuk <lakeuk@users.noreply.github.com>
2018-12-07 17:03:17 +0000

    Add files via upload

---
+++ services_off.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+import os
+
+os.system("systemctl disable gpioRuneaudioi2c")
+os.system("systemctl disable gpioRuneaudioi2cRotary")
+os.system("systemctl disable ePaperRuneaudio")
+
+os.system("systemctl stop gpioRuneaudioi2c")
+os.system("systemctl stop gpioRuneaudioi2cRotary")
+os.system("systemctl stop ePaperRuneaudio")
+
+os.system("rm /usr/lib/systemd/system/ePaperRuneaudio.service")
+os.system("rm /usr/lib/systemd/system/gpioRuneaudioi2c.service")
+os.system("rm /usr/lib/systemd/system/gpioRuneaudioi2cRotary.service")
+
---
+++ services_on.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import os
+
+os.system("cp /home/myapp/ePaperRuneaudio.service /usr/lib/systemd/system")
+os.system("cp /home/myapp/gpioRuneaudioi2c.service /usr/lib/systemd/system")
+os.system("cp /home/myapp/gpioRuneaudioi2cRotary.service /usr/lib/systemd/system")
+
+os.system("systemctl start gpioRuneaudioi2c")
+os.system("systemctl start gpioRuneaudioi2cRotary")
+os.system("systemctl start ePaperRuneaudio")
+
+os.system("systemctl enable gpioRuneaudioi2c")
+os.system("systemctl enable gpioRuneaudioi2cRotary")
+os.system("systemctl enable ePaperRuneaudio")
