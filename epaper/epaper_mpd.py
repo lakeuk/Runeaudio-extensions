091d5f7a7e2ce734a936c7efe345842f457c37e0
lakeuk <lakeuk@users.noreply.github.com>
2019-08-20 20:06:57 +0100

    Updated to Python3

--- epaper/epaper_mpd.py
+++ epaper/epaper_mpd.py
@@ -12,10 +12,13 @@ from mpd import (MPDClient, CommandError)
 from socket import error as SocketError
 
 #epaper imports
+import logging
 import epd2in9
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
+#logging.basicConfig(level=logging.DEBUG)
 
 pathbmp = '/home/myapp/epaper/bmp/'
 #arrbmp = ['BBC Radio 1','BBC Radio 2','BBC Radio 4 FM','BBC Radio 4 Extra','BBC Radio 5 Live UK','BBC 6 Music','LBC UK','Forth 1','Rock FM','Virgin Radio UK','Lakeland Radio 100.1, 100.8 & 101.4 FM','The Bay 96.9 FM','BBC Radio Cumbria','BBC Radio Lancashire','BBC Radio Manchester','Smooth Lake District','Heart North Lancs & Cumbria','Bay Trust Radio','CandoFM','Key 103']
@@ -29,8 +32,8 @@ def epaperReset():
   epd.init(epd.lut_full_update)
   epd.Clear(0xFF)  
   # For simplicity, the arguments are explicit numerical coordinates
-  image = Image.new('1', (epd2in9.EPD_WIDTH, epd2in9.EPD_HEIGHT), 255)  # 255: clear the frame
-  epd.display(epd.getbuffer(image)) 
+  #image = Image.new('1', (epd2in9.EPD_WIDTH, epd2in9.EPD_HEIGHT), 255)  # 255: clear the frame
+  #epd.display(epd.getbuffer(image)) 
 
 def drawbmpFull(imagename):
   epd = epd2in9.EPD()
@@ -60,156 +63,177 @@ CON_ID = {'host':HOST, 'port':PORT}
 
 ## Some functions
 def mpdConnect(client, con_id):
-    """
-    Simple wrapper to connect MPD.
-    """
-    try:
-        client.connect(**con_id)
-    except SocketError:
-        return False
-    return True
+  """
+  Simple wrapper to connect MPD.
+  """
+  try:
+    client.connect(**con_id)
+  except SocketError:
+    return False
+  return True
 
 def mpdAuth(client, secret):
-    """
-    Authenticate
-    """
-    try:
-        client.password(secret)
-    except CommandError:
-        return False
-    return True
+  """
+  Authenticate
+  """
+  try:
+    client.password(secret)
+  except CommandError:
+    return False
+  return True
 ##
 
 def main():
-    ## MPD object instance
-    client = MPDClient()
-    if mpdConnect(client, CON_ID):
-        print('Got connected!')
+## MPD object instance
+  client = MPDClient()
+  if mpdConnect(client, CON_ID):
+    print('Got connected!')
+  else:
+    print('fail to connect MPD server.')
+    sys.exit(1)
+
+# Auth if password is set non False
+  if PASSWORD:
+    if mpdAuth(client, PASSWORD):
+      print('Pass auth!')
     else:
-        print('fail to connect MPD server.')
-        sys.exit(1)
+      print('Error trying to pass auth.')
+      client.disconnect()
+      sys.exit(2)
 
-    # Auth if password is set non False
-    if PASSWORD:
-        if mpdAuth(client, PASSWORD):
-            print('Pass auth!')
+  laststation = 0
+  laststate = 0
+
+  while(1):
+
+    client.send_idle()
+    state = client.fetch_idle()
+
+    if (state[0] == 'mixer'):
+      print('Volume = ' + client.status()['volume'])
+      time.sleep(0.2)
+
+    if (state[0] == 'player'):
+      time.sleep(0.2)        
+
+    try:
+      #station = client.currentsong()['name']
+      station_url = subprocess.check_output("mpc -f %file% current", stderr=subprocess.STDOUT, shell=True) # http://mystreamurl/;?station_name=MY_STATION_NAME
+      station_url = station_url.decode('ISO-8859-1') # Resolves::TypeError: cannot use a string pattern on a bytes-like object
+      station = re.findall(r'\?station_name=(.*)', station_url)  # ['MY_STATION_NAME']
+      if len(station) > 0:
+        station = station[0].replace('_', ' ')  # MY STATION NAME
+      else:
+        station = ''
+      #print(station)
+    except KeyError:
+      station = ''
+
+    try:
+      state = client.status()['state']
+    except KeyError:
+      state = ''
+
+    try:
+      title = client.currentsong()['title']
+    except KeyError:
+      title = ''
+
+    try:
+      artist = client.currentsong()['artist']
+    except KeyError:
+      artist = ''
+
+    if(station != ''):    # webradio
+      #print('Station = ' + station)
+      if laststation != station:
+        #print(station)
+        if station in arrbmp:
+          print(station + ' - logo available')
+          #epaperReset()
+          #drawbmpPart(station)  
+          drawbmpFull(station)
         else:
-            print('Error trying to pass auth.')
-            client.disconnect()
-            sys.exit(2)
+          print(station + ' - no logo available')                  
+          #epaperReset()                    
+          #drawbmpPart('default')
+          drawbmpFull('default')                    
+      #print('Title = ' + title)
+        laststation = station
+      elif(laststate == 'stop' and state == 'play'):
+        if station in arrbmp:
+          print(station + ' - logo available - stop-play')
+          #epaperReset()
+          #drawbmpPart(station)   
+          drawbmpFull(station)
+        else:
+          print(station + ' - no logo available - stop-play')                  
+          #epaperReset()
+          #drawbmpPart('default')
+          drawbmpFull('default')                    
+      laststate = 'play'                  
+    else:                 # file
+      print('Title = ' + title)
+      print('Artist = ' + artist)
 
-    laststation = 0
-    laststate = 0
+    if(state == 'stop'):
+      print('State ' + state)
+      drawbmpFull('blank')        
+      #drawbmpPart('blank')                
+      laststate = 'stop'
 
-    while(1):
+    #if (state[0] == 'playlist'):
+    #  print('the current playlist has been modified')
 
-        client.send_idle()
-        state = client.fetch_idle()
+    #if (state[0] == 'database'): 
+    #  print('the song database has been modified after update')
 
-        if (state[0] == 'mixer'):
-            print('Volume = ' + client.status()['volume'])
-            time.sleep(0.2)
+    #if (state[0] == 'update'): 
+    #  print('a database update has started or finished. If the database was modified during the update, the database event is also emitted.')
 
-        if (state[0] == 'player'):
-            time.sleep(0.2)        
+    #if (state[0] == 'stored_playlist'): 
+    #  print('a stored playlist has been modified, renamed, created or deleted')
 
-            try:
-                #station = client.currentsong()['name']
-                station_url = subprocess.check_output("mpc -f %file% current", stderr=subprocess.STDOUT, shell=True) # http://mystreamurl/;?station_name=MY_STATION_NAME
-		station = re.findall(r'\?station_name=(.*)', station_url)  # ['MY_STATION_NAME']
-		if len(station) > 0:
-			station = station[0].replace('_', ' ')  # MY STATION NAME
-		else:
-			station = ''
-		#print station
-            except KeyError:
-                station = ''
+    #if (state[0] == 'output'): 
+    #  print('an audio output has been enabled or disabled')
 
-	    try:
-	    	state = client.status()['state']
-            except KeyError:
-                state = ''
-                
-            try:
-                title = client.currentsong()['title']
-            except KeyError:
-                title = ''
+    #if (state[0] == 'options'): 
+    #  print('options like repeat, random, crossfade, replay gain')
 
-            try:
-                artist = client.currentsong()['artist']
-            except KeyError:
-                artist = ''
+    #if (state[0] == 'sticker'): 
+    #  print('the sticker database has been modified.')
 
-            if(station != ''):    # webradio
-                #print('Station = ' + station)
-                if laststation != station:
-                  #print(station)
-                  if station in arrbmp:
-                    print(station + ' - logo available')
-                    #drawbmpPart('blank')
-		    epaperReset()                    
-                    drawbmpPart(station)  
-                    #drawbmpFull(station)                    
-                  else:
-                    print(station + ' - no logo available')                  
-                    #drawbmpPart('blank')
- 		    epaperReset()                    
-                    drawbmpPart('default')
-		    #drawbmpFull('default')                    
-                    #drawbmpPart('allblack')                    
-                #print('Title = ' + title)
-                  laststation = station
-            	elif(laststate == 'stop' and state == 'play'):
-            	  if station in arrbmp:
-                    print(station + ' - logo available - stop-play')
-                    #drawbmpPart('blank')
-                    epaperReset()
-                    drawbmpPart(station)   
-		    #drawbmpFull(station)
-                  else:
-                    print(station + ' - no logo available - stop-play')                  
-                    #drawbmpPart('blank')
-                    epaperReset()
-                    drawbmpPart('default')
-                    #drawbmpFull('default')                    
-                    #drawbmpPart('allblack') 
-                laststate = 'play'                  
-            else:                 # file
-                print('Title = ' + title)
-                print('Artist = ' + artist)
+  ## disconnect
+  client.disconnect()
+  print("end of script - client disconnected")
+  sys.exit(0)
 
-	    if(state == 'stop'):
-	    	print('State ' + state)
-                drawbmpFull('allblack')  
-                drawbmpPart('blank')                
-                #drawbmpPart('default')	
-                laststate = 'stop'
-		
-#        if (state[0] == 'playlist'):
-#            print('the current playlist has been modified')
+def test():
+  #try:
+    epaperReset()
+    print("step 1")
+    epaperReset()
+    print("step 2")    
+    epaperReset()
+    print("step 3")    
+    drawbmpPart('BBC Radio 1')
+    print("step 4")    
+    time.sleep(10)  
+    epaperReset()
+    print("step 5")    
+    drawbmpPart('BBC Radio 2')
+    print("step 6")    
+    time.sleep(2)  
 
-#        if (state[0] == 'database'): 
-#           print('the song database has been modified after update')
+  #except IOError as e:
+  #  logging.info(e)
 
-#        if (state[0] == 'update'): 
-#           print('a database update has started or finished. If the database was modified during the update, the database event is also emitted.')
-
-#        if (state[0] == 'stored_playlist'): 
-#           print('a stored playlist has been modified, renamed, created or deleted')
-
-#        if (state[0] == 'output'): 
-#           print('an audio output has been enabled or disabled')
-
-#        if (state[0] == 'options'): 
-#           print('options like repeat, random, crossfade, replay gain')
-
-#        if (state[0] == 'sticker'): 
-#           print('the sticker database has been modified.')
-
-    ## disconnect
-    client.disconnect()
-    sys.exit(0)
+  #except KeyboardInterrupt:    
+  #  logging.info("ctrl + c:")
+  #  epd2in9.epdconfig.module_exit()
+  #  exit()
 
 # Script starts here
 if __name__ == "__main__":
-    main()
+  main()
+  #test()
--- epaper/epd2in9.py
+++ epaper/epd2in9.py
@@ -1,33 +1,13 @@
-# /*****************************************************************************
-# * | File        :	  EPD_1in54.py
+# *****************************************************************************
+# * | File        :	  epd2in9.py
 # * | Author      :   Waveshare team
 # * | Function    :   Electronic paper driver
 # * | Info        :
 # *----------------
-# * |	This version:   V3.0
-# * | Date        :   2018-11-06
-# * | Info        :   python2 demo
-# * 1.Remove:
-#   digital_write(self, pin, value)
-#   digital_read(self, pin)
-#   delay_ms(self, delaytime)
-#   set_lut(self, lut)
-#   self.lut = self.lut_full_update
-# * 2.Change:
-#   display_frame -> TurnOnDisplay
-#   set_memory_area -> SetWindow
-#   set_memory_pointer -> SetCursor
-#   get_frame_buffer -> getbuffer
-#   set_frame_memory -> display
-# * 3.How to use
-#   epd = epd2in9.EPD()
-#   epd.init(epd.lut_full_update)
-#   image = Image.new('1', (epd1in54.EPD_WIDTH, epd1in54.EPD_HEIGHT), 255)
-#   ...
-#   drawing ......
-#   ...
-#   epd.display(getbuffer(image))
-# ******************************************************************************/
+# * | This version:   V4.0
+# * | Date        :   2019-06-20
+# # | Info        :   python demo
+# -----------------------------------------------------------------------------
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documnetation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -47,37 +27,12 @@
 # THE SOFTWARE.
 #
 
-
-import epdconfig
-from PIL import Image
-import RPi.GPIO as GPIO
+import logging
+import epdconfig  #was--> #from . import epdconfig
 
 # Display resolution
 EPD_WIDTH       = 128
 EPD_HEIGHT      = 296
-
-# EPD2IN9 commands
-DRIVER_OUTPUT_CONTROL                       = 0x01
-BOOSTER_SOFT_START_CONTROL                  = 0x0C
-GATE_SCAN_START_POSITION                    = 0x0F
-DEEP_SLEEP_MODE                             = 0x10
-DATA_ENTRY_MODE_SETTING                     = 0x11
-SW_RESET                                    = 0x12
-TEMPERATURE_SENSOR_CONTROL                  = 0x1A
-MASTER_ACTIVATION                           = 0x20
-DISPLAY_UPDATE_CONTROL_1                    = 0x21
-DISPLAY_UPDATE_CONTROL_2                    = 0x22
-WRITE_RAM                                   = 0x24
-WRITE_VCOM_REGISTER                         = 0x2C
-WRITE_LUT_REGISTER                          = 0x32
-SET_DUMMY_LINE_PERIOD                       = 0x3A
-SET_GATE_TIME                               = 0x3B
-BORDER_WAVEFORM_CONTROL                     = 0x3C
-SET_RAM_X_ADDRESS_START_END_POSITION        = 0x44
-SET_RAM_Y_ADDRESS_START_END_POSITION        = 0x45
-SET_RAM_X_ADDRESS_COUNTER                   = 0x4E
-SET_RAM_Y_ADDRESS_COUNTER                   = 0x4F
-TERMINATE_FRAME_READ_WRITE                  = 0xFF
 
 class EPD:
     def __init__(self):
@@ -89,71 +44,75 @@ class EPD:
         self.height = EPD_HEIGHT
 
     lut_full_update = [
-        0x02, 0x02, 0x01, 0x11, 0x12, 0x12, 0x22, 0x22, 
-        0x66, 0x69, 0x69, 0x59, 0x58, 0x99, 0x99, 0x88, 
-        0x00, 0x00, 0x00, 0x00, 0xF8, 0xB4, 0x13, 0x51, 
-        0x35, 0x51, 0x51, 0x19, 0x01, 0x00
+        0x50, 0xAA, 0x55, 0xAA, 0x11, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0xFF, 0xFF, 0x1F, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     ]
 
     lut_partial_update  = [
-        0x10, 0x18, 0x18, 0x08, 0x18, 0x18, 0x08, 0x00, 
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-        0x00, 0x00, 0x00, 0x00, 0x13, 0x14, 0x44, 0x12, 
+        0x10, 0x18, 0x18, 0x08, 0x18, 0x18,
+        0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x13, 0x14, 0x44, 0x12,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     ]
         
     # Hardware reset
     def reset(self):
-        epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        epdconfig.digital_write(self.reset_pin, 1)
         epdconfig.delay_ms(200) 
-        epdconfig.digital_write(self.reset_pin, GPIO.LOW)         # module reset
-        epdconfig.delay_ms(200)
-        epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        epdconfig.digital_write(self.reset_pin, 0)
+        epdconfig.delay_ms(10)
+        epdconfig.digital_write(self.reset_pin, 1)
         epdconfig.delay_ms(200)   
 
     def send_command(self, command):
-        epdconfig.digital_write(self.cs_pin, GPIO.LOW)
-        epdconfig.digital_write(self.dc_pin, GPIO.LOW)
+        epdconfig.digital_write(self.dc_pin, 0)
+        epdconfig.digital_write(self.cs_pin, 0)
         epdconfig.spi_writebyte([command])
-        epdconfig.digital_write(self.cs_pin, GPIO.HIGH)
+        epdconfig.digital_write(self.cs_pin, 1)
 
     def send_data(self, data):
-        epdconfig.digital_write(self.cs_pin, GPIO.LOW)
-        epdconfig.digital_write(self.dc_pin, GPIO.HIGH)
+        epdconfig.digital_write(self.dc_pin, 1)
+        epdconfig.digital_write(self.cs_pin, 0)
         epdconfig.spi_writebyte([data])
-        epdconfig.digital_write(self.cs_pin, GPIO.HIGH)
+        epdconfig.digital_write(self.cs_pin, 1)
         
-    def wait_until_idle(self):
-        while(epdconfig.digital_read(self.busy_pin) == 1):      # 0: idle, 1: busy
-            epdconfig.delay_ms(100)    
+    def ReadBusy(self):
+        while(epdconfig.digital_read(self.busy_pin) == 1):      #  0: idle, 1: busy
+            epdconfig.delay_ms(200) 
 
     def TurnOnDisplay(self):
-        self.send_command(DISPLAY_UPDATE_CONTROL_2)
+        self.send_command(0x22) # DISPLAY_UPDATE_CONTROL_2
         self.send_data(0xC4)
-        self.send_command(MASTER_ACTIVATION)
-        self.send_command(TERMINATE_FRAME_READ_WRITE)
+        self.send_command(0x20) # MASTER_ACTIVATION
+        self.send_command(0xFF) # TERMINATE_FRAME_READ_WRITE
         
-        self.wait_until_idle()
+        logging.debug("e-Paper busy")
+        self.ReadBusy()
+        logging.debug("e-Paper busy release")  
 
     def SetWindow(self, x_start, y_start, x_end, y_end):
-        self.send_command(SET_RAM_X_ADDRESS_START_END_POSITION)
+        self.send_command(0x44) # SET_RAM_X_ADDRESS_START_END_POSITION
         # x point must be the multiple of 8 or the last 3 bits will be ignored
         self.send_data((x_start >> 3) & 0xFF)
         self.send_data((x_end >> 3) & 0xFF)
-        self.send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)
+        self.send_command(0x45) # SET_RAM_Y_ADDRESS_START_END_POSITION
         self.send_data(y_start & 0xFF)
         self.send_data((y_start >> 8) & 0xFF)
         self.send_data(y_end & 0xFF)
         self.send_data((y_end >> 8) & 0xFF)
 
     def SetCursor(self, x, y):
-        self.send_command(SET_RAM_X_ADDRESS_COUNTER)
+        self.send_command(0x4E) # SET_RAM_X_ADDRESS_COUNTER
         # x point must be the multiple of 8 or the last 3 bits will be ignored
         self.send_data((x >> 3) & 0xFF)
-        self.send_command(SET_RAM_Y_ADDRESS_COUNTER)
+        self.send_command(0x4F) # SET_RAM_Y_ADDRESS_COUNTER
         self.send_data(y & 0xFF)
         self.send_data((y >> 8) & 0xFF)
-        self.wait_until_idle()
+        self.ReadBusy()
         
     def init(self, lut):
         if (epdconfig.module_init() != 0):
@@ -161,53 +120,56 @@ class EPD:
         # EPD hardware init start
         self.reset()
         
-        self.send_command(DRIVER_OUTPUT_CONTROL)
+        self.send_command(0x01) # DRIVER_OUTPUT_CONTROL
         self.send_data((EPD_HEIGHT - 1) & 0xFF)
         self.send_data(((EPD_HEIGHT - 1) >> 8) & 0xFF)
-        self.send_data(0x00)                     # GD = 0 SM = 0 TB = 0
-        self.send_command(BOOSTER_SOFT_START_CONTROL)
+        self.send_data(0x00) # GD = 0 SM = 0 TB = 0
+        
+        self.send_command(0x0C) # BOOSTER_SOFT_START_CONTROL 
         self.send_data(0xD7)
         self.send_data(0xD6)
         self.send_data(0x9D)
-        self.send_command(WRITE_VCOM_REGISTER)
-        self.send_data(0xA8)                     # VCOM 7C
-        self.send_command(SET_DUMMY_LINE_PERIOD)
-        self.send_data(0x1A)                     # 4 dummy lines per gate
-        self.send_command(SET_GATE_TIME)
-        self.send_data(0x08)                     # 2us per line
-        self.send_command(BORDER_WAVEFORM_CONTROL)
-        self.send_data(0x03)
-        self.send_command(DATA_ENTRY_MODE_SETTING)
-        self.send_data(0x03)                     # X increment Y increment
         
-        self.send_command(WRITE_LUT_REGISTER)
+        self.send_command(0x2C) # WRITE_VCOM_REGISTER
+        self.send_data(0xA8) # VCOM 7C
+        
+        self.send_command(0x3A) # SET_DUMMY_LINE_PERIOD
+        self.send_data(0x1A) # 4 dummy lines per gate
+        
+        self.send_command(0x3B) # SET_GATE_TIME
+        self.send_data(0x08) # 2us per line
+        
+        self.send_command(0x11) # DATA_ENTRY_MODE_SETTING
+        self.send_data(0x03) # X increment Y increment
+        
+        self.send_command(0x32) # WRITE_LUT_REGISTER
         for i in range(0, len(lut)):
             self.send_data(lut[i])
         # EPD hardware init end
         return 0
 
     def getbuffer(self, image):
-        # print "bufsiz = ",(self.width/8) * self.height
-        buf = [0xFF] * ((self.width/8) * self.height)
+        # logging.debug("bufsiz = ",int(self.width/8) * self.height)
+        buf = [0xFF] * (int(self.width/8) * self.height)
         image_monocolor = image.convert('1')
         imwidth, imheight = image_monocolor.size
         pixels = image_monocolor.load()
-        # print "imwidth = %d, imheight = %d",imwidth,imheight
+        # logging.debug("imwidth = %d, imheight = %d",imwidth,imheight)
         if(imwidth == self.width and imheight == self.height):
-            print "Vertical"
+            logging.debug("Vertical")
             for y in range(imheight):
                 for x in range(imwidth):
                     # Set the bits for the column of pixels at the current position.
                     if pixels[x, y] == 0:
-                        buf[(x + y * self.width) / 8] &= ~(0x80 >> (x % 8))
+                        buf[int((x + y * self.width) / 8)] &= ~(0x80 >> (x % 8))
         elif(imwidth == self.height and imheight == self.width):
-            print "Horizontal"
+            logging.debug("Horizontal")
             for y in range(imheight):
                 for x in range(imwidth):
                     newx = y
                     newy = self.height - x - 1
                     if pixels[x, y] == 0:
-                        buf[(newx + newy*self.width) / 8] &= ~(0x80 >> (y % 8))
+                        buf[int((newx + newy*self.width) / 8)] &= ~(0x80 >> (y % 8))
         return buf
 
     def display(self, image):
@@ -216,23 +178,24 @@ class EPD:
         self.SetWindow(0, 0, self.width - 1, self.height - 1)
         for j in range(0, self.height):
             self.SetCursor(0, j)
-            self.send_command(WRITE_RAM)
-            for i in range(0, self.width / 8):
-                self.send_data(image[i + j * (self.width / 8)])   
+            self.send_command(0x24) # WRITE_RAM
+            for i in range(0, int(self.width / 8)):
+                self.send_data(image[i + j * int(self.width / 8)])   
         self.TurnOnDisplay()
         
     def Clear(self, color):
         self.SetWindow(0, 0, self.width - 1, self.height - 1)
         for j in range(0, self.height):
             self.SetCursor(0, j)
-            self.send_command(WRITE_RAM)
-            for i in range(0, self.width / 8):
+            self.send_command(0x24) # WRITE_RAM
+            for i in range(0, int(self.width / 8)):
                 self.send_data(color)   
         self.TurnOnDisplay()
 
     def sleep(self):
-        self.send_command(DEEP_SLEEP_MODE)
+        self.send_command(0x10) # DEEP_SLEEP_MODE
         self.send_data(0x01)
-
+        
+        epdconfig.module_exit()
 ### END OF FILE ###
 
